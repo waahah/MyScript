1b767237b2003abd0b29b770d505451b08c6691a
laijinwei <18776666967@qq.com>
2024-04-09 20:00:51 +0800

    feature: 修复微信公众号的文章保存下来没有图片的bug

--- htmlToMarkdown/htmlToMarkdown.user.js
+++ htmlToMarkdown/htmlToMarkdown.user.js
@@ -536,6 +536,11 @@ var TurndownService = (function () {
             var src = node.getAttribute('src') || '';
             var title = cleanAttribute(node.getAttribute('title'));
             var titlePart = title ? ' "' + title + '"' : '';
+
+            if (document.URL.indexOf('weixin.qq.com') >= 0) {
+                src = node.getAttribute('data-src') || '';
+            }
+
             return src ? '![' + alt + ']' + '(' + src + titlePart + ')' : ''
         }
     };
